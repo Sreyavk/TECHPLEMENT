c41a136ce6d8d286751f9957f3678cd3bf989369
Sreyavk <sreyavk159@gmail.com>
2024-08-11 22:25:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,5 +52,5 @@ app.get('/api/quotes/search', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${5000}`);
 });
