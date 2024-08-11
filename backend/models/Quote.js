c71362e6e581a94cee9e9d17728cd5f3e280ebd6
Sreyavk <sreyavk159@gmail.com>
2024-08-11 22:03:46 +0530

    Quote.js

--- backend/models/Quote.js
+++ backend/models/Quote.js
@@ -1,19 +1,1 @@
-const mongoose = require('mongoose');
 
-const quoteSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    }
-});
-
-module.exports = mongoose.model('Quote', quoteSchema);
